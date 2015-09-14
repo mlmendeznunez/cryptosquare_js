992d971e3114b5c699771352861deca3983062d8
Momo Ozawa & mlmendeznunez <student@epicodus.com>
2015-09-14 14:16:38 -0700

    Update inner loop of encrypt method to increment by width

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
+// Encrypter
+var encrypt = function(phrase) {
+  var strippedPhrase = punctuationLess(phrase);
+  var phraseLength = strippedPhrase.length;
+  var width = Math.ceil(Math.sqrt(phraseLength));
+  var encryptedString = "";
+
+  for (var i = 0; i < width; i++) {
+    for (var j = i; j < phraseLength; j += width) {
+      encryptedString += strippedPhrase[j];
+    }
+  }
+  return insertSpaces(5, encryptedString);
+};
+
 // Strip all whitespace and punctuation, and make everything lowercase
 var punctuationLess = function(phrase) {
   var strippedPhrase = phrase.replace(/[^A-Za-z0-9-]/g,"").toLowerCase();
@@ -16,29 +31,14 @@ var insertSpaces = function(n, phrase) {
   return finalPhrase;
 }
 
-// Encrypter
-var encrypt = function(phrase) {
-  var strippedPhrase = punctuationLess(phrase);
-  var phraseLength = strippedPhrase.length;
-  var width = Math.ceil(Math.sqrt(phraseLength));
-  var encryptedString = "";
-
-  for (var i = 0; i < width; i++) {
-    for (var j = i; j < phraseLength; j += 4) {
-      encryptedString += strippedPhrase[j];
-    }
-  }
-  return insertSpaces(5, encryptedString);
-};
-
 $(document).ready(function() {
-  $("form#factorial").submit(function(event) {
-    var number = parseInt($("input#number").val());
-    var result = rFactorial(number);
+  $("form#cryptosquare").submit(function(event) {
+    var initialPhrase = $("input#initialPhrase").val();
+    var encryptedPhrase = encrypt(initialPhrase);
 
     // Populate data into number and factorialResult span field respectively
-    $(".number").text(number);
-    $(".factorialResult").text(result);
+    $(".initialPhrase").text(initialPhrase);
+    $(".encryptedPhrase").text(encryptedPhrase);
 
     $("#result").show();
     event.preventDefault();
